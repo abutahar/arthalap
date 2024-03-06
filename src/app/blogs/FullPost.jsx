e6aa26bc41ecc6023dbd6cb9eeaded5bebff0106
abutahar <mdabutaharmondol@gmail.com>
2024-03-06 15:51:36 +0600

    edit

--- src/app/blogs/FullPost.jsx
+++ src/app/blogs/FullPost.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const FullPost = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h1>currrently empty</h1>
+    </div>
+  );
 };
 
 export default FullPost;
